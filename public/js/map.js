3aac7dfe5bc2f674ad51f6384e2792e4983d2519
Naghma Taj <naghmas.taj@gmail.com>
2025-07-31 17:08:41 +0530

    Add Project Files

---
+++ public/js/map.js
@@ -0,0 +1,19 @@
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: listing.geometry.coordinates, // starting position [lng, lat].
+    zoom: 9 // starting zoom
+});
+
+// console.log(coordinates);
+
+const marker = new mapboxgl.Marker({color: 'red'})
+        .setLngLat(listing.geometry.coordinates) //listing geometry cordinated
+        .setPopup(
+            new mapboxgl.Popup({ offset: 25})
+            .setHTML(
+                `<h4>${listing.location}</h4><p> Exact location will be provided after booking!</p>`
+            )
+        )
+        .addTo(map);
